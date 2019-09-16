b89398bf41ff47b5839294c3e56c1515672c823a
pageisfound <52653911+pageisfound@users.noreply.github.com>
2019-09-16 16:25:17 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,13 @@
+$(function() {
+    let productType = $('#product-type');
+    let productAttribute = $('.product-attribute');
 
+    productAttribute.hide();
+
+    productType.change(function() {
+        productAttribute.hide();
+        if (productType.val() !== 'none') {
+            $('#' + productType.val() + '-attribute').show();
+        }
+    });
+});
